348c308901fe7d4e513de0ec0f703ab4e651173d
Andi Liu <awliu2@uchicago.edu>
2023-12-07 12:52:03 -0600

    finished project code

--- src/Board.hs
+++ src/Board.hs
@@ -1,74 +1,103 @@
+{-# LANGUAGE OverloadedStrings #-} 
 module Board where
-
-import Data.List ( filter, foldl', take, nub )
-import Data.Set
-import System.Random
+import Data.List ( filter, foldl', take, nub, concatMap )
+import Data.Set as Set
+import System.Random ( getStdGen, Random(randomRs) )
+import Control.Applicative
+import Control.Monad
+import Data.Aeson 
 import Debug.Trace ( trace )
+
 type Position = (Int, Int)
 
--- | Get all adjacent positions.
+-- | Get a list of all adjacent positions.
 -- | does not check if the positions is on the board
-adjacent :: Position  -> [Position]
-adjacent (x, y) = [(x + dx, y + dy) | dx <- [-1..1], dy <- [-1..1], dx /= 0 || dy /= 0]
+adjacent :: Position  -> Set Position 
+adjacent (r, c) = Set.fromList [(r + dr, c + dc) | dr <- [-1..1], dc <- [-1..1], dr /= 0 || dc /= 0]
 
+{-# LANGUAGE OverloadedStrings #-}
 -- | Board data structure.
 data Board = Board {
-    width :: Int,
-    height :: Int,
+    nr :: Int,
+    nc :: Int,
     bombs :: Set Position,
     revealed :: Set Position,
     marked :: Set Position
-}
+} deriving (Eq)
+
+instance FromJSON Board where
+    parseJSON = withObject "Board" $ \v -> Board
+        <$> v .: "nr"
+        <*> v .: "nc"
+        <*> v .: "bombs"
+        <*> v .: "revealed"
+        <*> v .: "marked"
+
+instance ToJSON Board where
+    toJSON (Board nr nc bombs revealed marked) =
+        object [ "nr" .= nr
+               , "nc" .= nc
+               , "bombs" .= bombs
+               , "revealed" .= revealed
+               , "marked" .= marked
+               ]
+    toEncoding (Board nr nc bombs revealed marked) =
+        pairs $  "nr" .= nr
+               <> "nc" .= nc
+               <> "bombs" .= bombs
+               <> "revealed" .= revealed
+               <> "marked" .= marked
 
 -- | Initialize an empty board with the given dimensions.
 initBoard :: Int -> Int -> Board
-initBoard width height = Board {
-    width = width,
-    height = height,
-    bombs = empty,
-    revealed = empty,
-    marked = empty
+initBoard nr nc = Board {
+    nr = nr,
+    nc = nc,
+    bombs = Set.empty,
+    revealed = Set.empty,
+    marked = Set.empty
 }
 
 instance Show Board where
     show :: Board -> String
-    show board = unlines $ [showRow y | y <- [0..height board - 1]]
+    show board = unlines $ [showRow r | r <- [0..nr board - 1]]
         where
             showRow :: Int -> String
-            showRow y = concat [showPosition (x, y) | x <- [0..width board - 1]]
+            showRow r = concat [showPosition (r, c) | c <- [0..nc board - 1]]
             showPosition :: Position -> String
             showPosition pos
-                | pos `Data.Set.member` revealed board = showRevealed pos
-                | pos `Data.Set.member` marked board = showMarked pos
+                | isRevealed pos board = showRevealed pos
+                | isMarked pos board = showMarked pos
                 | otherwise = showHidden pos
             showRevealed :: Position -> String
             showRevealed pos
-                | pos `Data.Set.member` bombs board = "[*]"
+                | pos `Set.member` bombs board = "[*]"
                 | otherwise = showAdjacentBombs pos
             showMarked :: Position -> String
             showMarked _ = "[X]"
             showHidden :: Position -> String
-            showHidden _ = "[-]"
+            showHidden _ = "[ ]"
             showAdjacentBombs :: Position -> String
             showAdjacentBombs pos = case numAdjacentBombs pos board of
-                n -> "[" ++ show n ++ "]"
+                0 -> "   "
+                n -> " " ++ show n ++ " "
          
 
 -- | Set a single bomb at the given position.
 -- | This function is idempotent.
 setBomb :: Position -> Board -> Board
 setBomb pos board = board {
-    bombs = Data.Set.insert pos (bombs board)
+    bombs = Set.insert pos (bombs board)
 }
 
 -- | Returns the number of bombs adjacent to the given position.
 numAdjacentBombs :: Position -> Board -> Int
-numAdjacentBombs pos board = length $ Data.List.filter (`Data.Set.member` bombs board) $ adjacent pos
+numAdjacentBombs pos board = Set.size $ Set.filter (`Set.member` bombs board) $ adjacent pos
 
 -- | Generate n random positions for bombs
 randomPositions :: Int -> Int -> Int -> IO [Position]
-randomPositions width height numBombs = 
-    let positions = [(x, y) | x <- [0..width - 1], y <- [0..height - 1]]
+randomPositions nr nc numBombs = 
+    let positions = [(x, y) | x <- [0..nr - 1], y <- [0..nc - 1]]
     in do
         gen <- getStdGen
         return $ Data.List.take numBombs $ nub $ randomRs (0, length positions - 1) gen >>= return . (positions !!)
@@ -79,65 +108,102 @@ setBombs positions board = Data.List.foldl' (flip setBomb) board positions
 
 setNRandomBombs :: Int -> Board -> IO Board
 setNRandomBombs numBombs board = do
-    positions <- randomPositions (width board) (height board) numBombs
-    trace (show positions) $ return $ setBombs positions board
-
+    positions <- randomPositions (nr board) (nc board) numBombs
+    return $ setBombs positions board
 
 -- Game State Implementations --
 
+-- | Returns the number of currently marked squares
+numMarked :: Board -> Int
+numMarked board = Set.size (marked board)
+
+-- | Returns the number of bombs on the board
+numBombs :: Board -> Int
+numBombs board = Set.size (bombs board)
+
 -- | Returns true if the given position is revealed.
 isRevealed :: Position -> Board -> Bool
-isRevealed pos board = pos `Data.Set.member` revealed board
+isRevealed pos board = pos `Set.member` revealed board
 
 -- | Returns true if the given position is marked.
 isMarked :: Position -> Board -> Bool
-isMarked pos board = pos `Data.Set.member` marked board
+isMarked pos board = pos `Set.member` marked board
 
--- Returns true if the given position is a bomb.
+-- | Returns true if the given position is a bomb.
 isBomb :: Position -> Board -> Bool
-isBomb pos board = pos `Data.Set.member` bombs board
+isBomb pos board = pos `Set.member` bombs board
+
+-- | Returns true if the given position is on the board.
+isOnBoard :: Position -> Board -> Bool
+isOnBoard pos board = fst pos >= 0 && fst pos < nr board && snd pos >= 0 && snd pos < nc board
 
 -- | Returns true if the game is lost.
 -- the game is lost if any bomb is revealed
 isGameLost :: Board -> Bool
-isGameLost board = Data.Set.intersection (bombs board) (revealed board) /= empty
+isGameLost board = Set.intersection (bombs board) (revealed board) /= Set.empty
 
 -- | Returns true if the game is won.
 -- The game is won if all bombs are marked.
 isGameWon :: Board -> Bool
-isGameWon board = Data.Set.isSubsetOf (bombs board) (marked board)
+isGameWon board = Set.isSubsetOf (bombs board) (marked board)
 
 -- Move Implementations --
-data Move = Reveal Position | Mark Position | Unmark Position
+data Move = Reveal Position | Mark Position deriving (Eq, Show)
     
 -- | play a move on the board
 play :: Move -> Board -> Board
-play (Reveal pos) = revealPosition pos
-play (Mark pos) = markPosition pos
-play (Unmark pos) = unmarkPosition pos
+play (Reveal pos) b = revealPosition False pos b
+play (Mark pos) b = if isMarked pos b then unmarkPosition pos b else markPosition pos b
 
 -- | Reveal the given position.
 -- If the position is not on the board, do nothing.
--- If the position is already revealed, do nothing.
+-- If the position is already revealed, this reveals all adjacent positions that are not marked (which could be bombs)
 -- If the position is marked, do nothing.
 -- If the position is a bomb, reveal only the given position. (Game over)
 -- Otherwise, reveal only the given position.
-revealPosition :: Position -> Board -> Board
-revealPosition pos board
-    | fst pos < 0 || fst pos >= width board || snd pos < 0 || snd pos >= height board = board
-    | pos `Data.Set.member` revealed board = board
-    | pos `Data.Set.member` marked board = board
-    | pos `Data.Set.member` bombs board = board
-    | otherwise = board {
-        revealed = Data.Set.insert pos (revealed board)
-    }
+revealPosition :: Bool -> Position -> Board -> Board
+revealPosition ignoreBombs pos board 
+    | not $ isOnBoard pos board = board
+    | isRevealed pos board && not ignoreBombs = do
+        -- reveal all adjacent positions that are not marked
+        let adjacentPositions = Set.filter (\p -> not (isRevealed p board) && not (isMarked p board) && isOnBoard p board) (adjacent pos)
+        -- if any of the adjacent positions are bombs, reveal all bombs
+        trace (show adjacentPositions) $ if Set.size (Set.intersection adjacentPositions (bombs board)) > 0 then revealAllBombs board
+        else do 
+        -- otherwise, reveal all adjacent positions while ignoring bombs that are adjacent to the adjacent positions
+         Set.foldl' (flip (revealPosition True)) board adjacentPositions
+    | isMarked pos board = board
+    | pos `Set.member` bombs board = if ignoreBombs then board else revealAllBombs board
+    | otherwise = revealRecursive pos board 
+-- | Reveal just given position.
+revealOnlyPosition :: Position -> Board -> Board
+revealOnlyPosition pos board = board {
+    revealed = Set.insert pos (revealed board)  
+}
 
--- given a position with no adjacent bombs, recursively get all adjacent positions with no adjacent bombs
--- if a position does have a bomb adjacent, do not get any of its adjacent positions
-getAllSafeAdjacentPositions :: Position -> Board -> [Position]
-getAllSafeAdjacentPositions pos board
-    | numAdjacentBombs pos board /= 0 = []
-    | otherwise = pos : concat [getAllSafeAdjacentPositions pos' board | pos' <- adjacent pos, pos' `Data.Set.notMember` revealed board]
+
+-- | Reveal the given position and all adjacent positions with no adjacent bombs.
+revealRecursive :: Position -> Board -> Board
+revealRecursive pos board = board {
+    revealed = Set.insert pos (Set.union (revealed board) (getAllSafeAdjacentPositions pos board Set.empty))
+}
+
+-- | Given a position with no adjacent bombs, recursively get all adjacent positions with no adjacent bombs
+-- the positions must not be revealed, marked, or bombs and the positions must be on the board
+getAllSafeAdjacentPositions :: Position -> Board -> Set Position -> Set Position
+getAllSafeAdjacentPositions pos board acc = do
+    if numAdjacentBombs pos board > 0 then acc else do
+        let adjacentPositions = Set.filter (\p -> not (isRevealed p board) && not (isMarked p board) && not (isBomb p board) && isOnBoard p board) (adjacent pos)
+        let newAcc = acc `union` adjacentPositions
+        if Set.size newAcc == Set.size acc then acc else Set.foldl' (\acc' p -> getAllSafeAdjacentPositions p board acc') newAcc adjacentPositions
+
+-- | Reveal all bombs.
+--  This is called when the player loses.
+revealAllBombs :: Board -> Board
+revealAllBombs board = board {  
+    revealed = Set.union (bombs board) (revealed board)
+}
+
 
 -- | Mark the given position.
 -- | If the position is not on the board, do nothing.
@@ -146,10 +212,10 @@ getAllSafeAdjacentPositions pos board
 -- | Otherwise, mark the position.
 markPosition :: Position -> Board -> Board
 markPosition pos board
-    | pos `Data.Set.member` marked board = board
-    | pos `Data.Set.member` revealed board = board
+    | pos `Set.member` marked board = board
+    | pos `Set.member` revealed board = board
     | otherwise = board {
-        marked = Data.Set.insert pos (marked board)
+        marked = Set.insert pos (marked board)
     }
 
 -- | Unmark the given position.
@@ -157,7 +223,9 @@ markPosition pos board
 -- | Otherwise, unmark the position.
 unmarkPosition :: Position -> Board -> Board
 unmarkPosition pos board
-    | pos `Data.Set.member` marked board = board {
-        marked = Data.Set.delete pos (marked board)
+    | pos `Set.member` marked board = board {
+        marked = Set.delete pos (marked board)
     }
     | otherwise = board
+
+    
--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,426 @@
 module Main where
-
 import Board
+    ( play,
+      isGameWon,
+      isGameLost,
+      setNRandomBombs,
+      initBoard,
+      Move(..),
+      Position,
+      Board(nc, nr),
+      numMarked,
+      numBombs )
+import System.IO (stdin, hReady, hSetEcho, hSetBuffering, BufferMode(NoBuffering, LineBuffering), nativeNewline)
+import System.Console.ANSI
+import System.Directory (doesFileExist, getDirectoryContents, renameFile, removeFile)
+import System.Exit
+import Data.Aeson
+import qualified Data.ByteString.Lazy as B
 
-data Action = KeyPress Char
 
+data Model = Model 
+    {
+    board :: Board,
+    cursor :: Position
+    } deriving (Eq)
+    
+type Action = [Char]
 
+data Difficulty = Easy | Medium | Hard deriving (Eq, Show)
+toDifficulty :: Int -> Difficulty
+toDifficulty 0 = Easy
+toDifficulty 1 = Medium
+toDifficulty _ = Hard
 
 main :: IO ()
 main = do
-    print "Hello, World!"
-    let board = initBoard 10 10
-    board' <- setNRandomBombs 10 board
-    print ("Bombs " ++ show (bombs board'))
-    -- play a move
-    print board'
-    let board'' = play (Reveal (0, 0)) board
-    print board''
+    hSetEcho stdin False
+    hSetBuffering stdin NoBuffering
+    c <- menuController "Welcome to Minesweeper!" ["New Game", "Load Game", "Manage Save Data", "Help", "Quit"]
+    case c of
+        0 -> playGame
+        1 -> do
+            model <- loadGame (Model (initBoard 0 0) (0, 0))
+            if model == Model (initBoard 0 0) (0, 0) then do
+                main
+            else do
+                controller model
+                return ()
+        2 -> do
+            manageSaveData
+            main
+        3 -> do
+            clearScreen
+            putStrLn helpMsg
+            putStrLn "Press any key to return to main menu..."
+            getKey
+            main
+        4 -> do
+            exit <- menuController "Are you sure you want to quit?" ["Yes", "No"]
+            if exit == 0 then do
+                putStrLn "Exiting game..."
+                exitSuccess
+            else do
+                main
+        _ -> do
+            putStrLn "Invalid action!" -- should never happen
+            main
+    return ()
 
-    -- play another move
-    let board''' = play (Mark (0, 1)) board''
-    print board'''
+
+playGame :: IO ()
+playGame = do
+    clearScreen
+    difficulty <- menuController "Select a difficulty:" ["Easy", "Medium", "Hard", "Cancel"]
+    if difficulty == 3 then do
+        putStrLn "Game cancelled!"
+        hSetEcho stdin False
+        hSetBuffering stdin NoBuffering
+        main
+    else do
+        model <- initModel (toDifficulty difficulty)
+        controller model
+        return ()
+
+initModel :: Difficulty -> IO Model
+initModel difficulty
+    | difficulty == Easy = do
+        let nr = 8
+            nc = 8
+            nb = 10
+        board <- setNRandomBombs nb (initBoard nr nc)
+        return (Model board (0, 0))
+    | difficulty == Medium = do
+        let nr = 16
+            nc = 16
+            nb = 40
+        board <- setNRandomBombs nb (initBoard nr nc)
+        return (Model board (0, 0))
+    | difficulty == Hard = do
+        let nr = 16
+            nc = 31
+            nb = 99
+        board <- setNRandomBombs nb (initBoard nr nc)
+        return (Model board (0, 0))
 
 
+controller :: Model -> IO Model
+controller model = do
+    clearScreen
+    hSetEcho stdin False
+    hSetBuffering stdin NoBuffering
+    printModel model
+    if isGameWon (board model) then do
+        putStrLn "You won!"
+        return model
+    else if isGameLost (board model) then do
+        putStrLn "You lost!"
+        return model
+    else do
+        key <- getKey
+        if key == "p" || key == "\ESC" then do
+            model' <- pauseGame model
+            controller model'
+        else if key == "q" then do
+            exitSuccess
+            -- print key
+            -- print "updating
+        else do 
+            let model' = update key model
+            controller model'
+
+getKey :: IO [Char]
+getKey = reverse <$> getKey' ""
+  where getKey' chars = do
+          char <- getChar
+          more <- hReady stdin
+          (if more then getKey' else return) (char:chars)
+
+update :: Action -> Model -> Model
+update action model
+    | action == "q" = model
+    | action == "w" || action == "\ESC[A" = moveCursor model (-1) 0
+    | action == "s" || action == "\ESC[B" = moveCursor model 1 0
+    | action == "a" || action == "\ESC[D" = moveCursor model 0 (-1) 
+    | action == "d" || action == "\ESC[C" = moveCursor model 0 1
+    | action == "r" || action == " " = playMove (Reveal (cursor model)) model
+    | action == "m" || action == "x" = playMove (Mark (cursor model)) model
+    | otherwise = model
+
+-----------------------------------------
+-- Menu Controller --
+
+-- | menuController is a controller for menus
+-- It takes a prompt and a list of options and returns the index of the selected option
+menuController :: String -> [String] -> IO Int
+menuController prompt options =do
+    hSetEcho stdin False
+    hSetBuffering stdin NoBuffering
+    selection <- menuController' prompt options 0
+    hSetEcho stdin True
+    hSetBuffering stdin LineBuffering
+    return selection
+
+menuController' :: String -> [String] -> Int -> IO Int
+menuController' prompt options selected = do
+    clearScreen
+    putStrLn prompt
+    printOptions options selected
+    let numOptions = length options
+    key <- getKey
+    if key == "w" || key == "\ESC[A" then do
+        -- move up
+        let selected' = (selected - 1) `mod` numOptions
+        menuController' prompt options selected'
+    else if key == "s" || key == "\ESC[B" then do
+        -- move down
+        let selected' = (selected + 1) `mod` numOptions
+        menuController' prompt options selected'
+    else if key == " " || key == "\n" then do
+        -- select with space or enter
+        return selected
+    else do
+        -- invalid key
+        menuController' prompt options selected
+
+-- | printOptions prints a list of options with the selected option highlighted
+printOptions :: [String] -> Int -> IO ()
+printOptions options selected = do
+    let options' = zipWith (\i o -> if i == selected then "> " ++ o else "  " ++ o) [0..] options
+    mapM_ putStrLn options'
+
+helpMsg = "IN GAME CONTROLS:\n\
+\* Use WASD or arrow keys to move\n\
+\* Press 'R' or spacebar to reveal\n\
+\* Press 'M' or 'X' to mark\n\
+\* Press P or ESC to pause\n"
+
+-- | pauseGame is a controller for the pause menu
+gamePausedMsg = "===GAME PAUSED===\n"
+pauseGame :: Model -> IO Model
+pauseGame model = do
+    clearScreen
+    choice <- menuController gamePausedMsg ["Resume", "Help", "Quit", "Save", "Load", "Manage Save Data"]
+    case choice of
+        0 -> do
+            hSetEcho stdin False
+            hSetBuffering stdin NoBuffering
+            return model
+        1 -> do
+            clearScreen
+            putStrLn helpMsg
+            putStrLn "Press any key to return to pause menu..."
+            getKey
+            pauseGame model
+        2 -> do
+            exit <- menuController "Are you sure you want to quit?" ["Yes", "No"]
+            if exit == 0 then do
+                putStrLn "Exiting game..."
+                exitSuccess
+            else do
+                pauseGame model
+        3 -> do
+            saveGame (board model)
+            pauseGame model
+        4 -> do
+            model' <- loadGame model
+            pauseGame model'
+        5 -> do
+            manageSaveData
+            pauseGame model
+        _ -> do
+            putStrLn "Invalid action!" -- should never happen
+            pauseGame model
+
+-- Save Data Management --
+saveDataDir = "saveData"
+
+writeBoardToFile :: Board -> String -> IO ()
+writeBoardToFile board name = do
+    B.writeFile name (encode board)
+
+readBoardFromFile :: String -> IO Board
+readBoardFromFile name = do
+    file <- B.readFile name
+    case decode file of
+        Nothing -> error $ "Error loading game from " ++ name ++ "!"
+        Just b -> return b
+
+-- | manageSaveData allows users to rename and delete save files
+manageSaveData :: IO ()
+manageSaveData = do
+    clearScreen
+    hSetEcho stdin True
+    hSetBuffering stdin LineBuffering
     
+    putStrLn "Save files:"
+    files <- getDirectoryContents saveDataDir
+    let saveFiles = filter (\f -> take 1 f /= ".") files
+    selection <- menuController "Select a save file to manage:" (saveFiles ++ ["Exit Manager"])
+    if selection == length saveFiles then do
+        putStrLn "Manage cancelled!"
+        hSetEcho stdin False
+        hSetBuffering stdin NoBuffering
+        return ()
+    else do
+        let name = saveFiles !! selection
+        let filePath = saveDataDir ++ "/" ++ name
+        exists <- doesFileExist filePath
+        if exists then do
+            manageSaveDataFile filePath name
+            manageSaveData 
+        else do
+            putStrLn "File does not exist!"
+            putStrLn "Exit save data manager? (y/n)"
+            exit <- getLine
+            if exit == "y" then do
+                putStrLn "Exiting save data manager..."
+                hSetEcho stdin False
+                hSetBuffering stdin NoBuffering
+                return ()
+            else do
+                hSetEcho stdin False
+                hSetBuffering stdin NoBuffering
+                manageSaveData
+                return ()
+
+manageSaveDataFile :: String -> String -> IO ()
+manageSaveDataFile filePath name = do
+    -- prompt user for action
+    selection <- menuController "Select an action:" ["Rename", "Delete", "Cancel"]
+    case selection of
+        0 -> do
+            putStrLn "Enter a new name for the save file:"
+            newName <- getLine
+            let newFilePath = saveDataDir ++ "/" ++ newName
+            renameFile filePath newFilePath
+            putStrLn $ "Successfully renamed \"" ++ name ++ "\" to \"" ++ newName ++ "\"!"
+            return ()
+        1 -> do
+            delete <- menuController "Are you sure you want to delete this save file?" ["Yes", "No"]
+            if delete == 0 then do
+                removeFile filePath
+                putStrLn $ "Successfully deleted \"" ++ name ++ "\"!"
+            else do
+                putStrLn "Delete cancelled!"
+            return ()
+        2 -> do
+            putStrLn "Manage cancelled!"
+        _ -> do
+            putStrLn "Invalid action!"
+            putStrLn "Manage cancelled!"
+
+saveGame :: Board -> IO ()
+saveGame board = do
+    clearScreen
+    hSetEcho stdin True
+    hSetBuffering stdin LineBuffering
+    putStrLn "Enter a name for your save file:"
+    name <- getLine
+    if name == "" then do
+        putStrLn "Invalid name!"
+        saveGame board
+    else do 
+        let filePath = saveDataDir ++ "/" ++ name
+        exists <- doesFileExist filePath
+        if exists then do
+            overwrite <- menuController ("File " ++ name ++ " already exists!\nOverwrite?") ["Yes", "No"]
+            if overwrite == 0 then do
+                writeBoardToFile board filePath
+                clearScreen
+                putStrLn $ "Successfully saved game to file \"" ++ name ++ "\"!"
+            else do
+                clearScreen
+                putStrLn "Save cancelled!"
+        else do
+            writeBoardToFile board filePath
+            clearScreen
+            putStrLn $ "Successfully saved game to file \"" ++ name ++ "\"!"
+    
+    putStrLn "Press any key to continue..."
+    getKey
+    hSetEcho stdin False
+    hSetBuffering stdin NoBuffering
+    return ()
+
+loadGame :: Model -> IO Model
+loadGame prevModel = do
+    clearScreen
+    hSetEcho stdin True
+    hSetBuffering stdin LineBuffering
+    -- get all save files
+    putStrLn "Save files:"
+    files <- getDirectoryContents saveDataDir
+    let saveFiles = filter (\f -> take 1 f /= ".") files
+    choice <- menuController "Select a save file to load:" (saveFiles ++ ["Cancel"])
+    if choice == length saveFiles then do
+        putStrLn "Load cancelled!"
+        hSetEcho stdin False
+        hSetBuffering stdin NoBuffering
+        return prevModel
+    else do
+        let name = saveFiles !! choice
+        let filePath = saveDataDir ++ "/" ++ name
+        exists <- doesFileExist filePath
+        if not exists then do
+            putStrLn "File does not exist!"
+            exit <- menuController "File does not exist! Exit save data manager?" ["Yes", "No"]
+            if exit == 0 then do
+                putStrLn "Exiting save data manager..."
+                hSetEcho stdin False
+                hSetBuffering stdin NoBuffering
+                return prevModel
+            else do 
+                hSetEcho stdin False
+                hSetBuffering stdin NoBuffering
+                loadGame prevModel
+        else do
+            board <- readBoardFromFile filePath
+            putStrLn "Load Successful!"
+            putStrLn "Press any key to continue..."
+            return (Model board (0, 0))
+    
+
+
+playMove :: Move -> Model -> Model
+playMove move model = do
+    let board' = play move (board model)
+    Model board' (cursor model)
+
+moveCursor :: Model -> Int -> Int -> Model
+moveCursor model dr dc = do
+    let numRows = nr (board model)
+        numCols = nc (board model)
+        r' = (fst (cursor model) + dr) `mod` numRows
+        c' = (snd (cursor model) + dc) `mod` numCols
+        cursor' = (r', c')
+    Model (board model) cursor'
+
+
+-- Displaying the Game --
+yellowEscSeq = "\x1b[30;43m"
+endEscSeq = "\x1b[0m"
+charsPerCell = 3
+
+printModel :: Model -> IO ()
+printModel model = do
+    let boardStr = show (board model)
+        boardStrLen = length boardStr
+        numRows = nr (board model)
+        numCols = nc (board model)
+        charsPerLine = numCols * charsPerCell + 1 -- +1 for newline
+        cursorPos = cursor model
+        -- given cursorPos = (r, c):
+        -- cursorStart = r * charsPerLine + c * 3
+        -- cursorEnd = cursorStart + 3
+        cursorStart =  fst cursorPos * charsPerLine + snd cursorPos * charsPerCell
+        cursorEnd = cursorStart + 3
+    
+    putStrLn $ "BOMBS LEFT: " ++ show (numBombs (board model) - numMarked (board model))
+    putStrLn $ formatBoardString boardStr cursorStart cursorEnd
+    
+formatBoardString :: String -> Int -> Int -> String
+formatBoardString bs start end = do
+    let (before, after) = splitAt start bs
+        (cursor, rest) = splitAt (end - start) after
+    before ++ yellowEscSeq ++ cursor ++ endEscSeq ++ rest
